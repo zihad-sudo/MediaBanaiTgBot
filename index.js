6fa3b1d04e2443f600e8557e3e110a023d693a7a
zihad-sudo <zihad@zihadimtiase.com>
2025-12-09 08:23:10 +0600

    Update index.js

--- index.js
+++ index.js
@@ -4,25 +4,30 @@ const config = require('./src/config/settings');
 const logger = require('./src/utils/logger');
 const db = require('./src/utils/db');
 
-// UPDATED: Import new handlers
-const { handleMessage, handleCallback, handleAdmin, handleGroupMessage, handleStart, handleHelp } = require('./src/utils/handlers');
+// Imports
+const { handleMessage, handleCallback, handleGroupMessage, handleStart, handleHelp } = require('./src/utils/handlers');
+const { handleStats, handleBroadcast } = require('./src/utils/admin'); // NEW FILE
 const { setupServer } = require('./src/server/web');
 
 logger.init();
 if (!fs.existsSync(config.DOWNLOAD_DIR)) fs.mkdirSync(config.DOWNLOAD_DIR, { recursive: true });
 
+// Connect DB
 db.connect(); 
 
 const bot = new Telegraf(config.BOT_TOKEN);
 
-// --- PROFESSIONAL COMMANDS ---
+// --- COMMANDS ---
 bot.start(handleStart);
 bot.help(handleHelp);
 
-bot.command('stats', handleAdmin);
-bot.command('broadcast', handleAdmin);
+// Admin Commands (Now in separate file)
+bot.command('stats', handleStats);
+bot.command('broadcast', handleBroadcast);
 
+// Group & Text Logic
 bot.on('text', async (ctx, next) => {
+    // Check for Ghost Mentions first
     if (ctx.chat.type === 'group' || ctx.chat.type === 'supergroup') {
         await handleGroupMessage(ctx, () => handleMessage(ctx));
     } else {
